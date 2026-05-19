236c21f9cb8c1d49b890325f7e4cf9a677c9bd09
Lakshay Sablok <lakshaysablok@gmail.com>
2026-05-19 17:41:30 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length !== t.length)
+        return false
+        const map =new Map();
+        for(const c of s){
+            if(map.has(c)) map.set(c, map.get(c) + 1)
+            else map.set(c,1)
+        }
+        for(const c of t){
+            if(!map.has(c)) return false;
+            map.set(c,map.get(c) - 1)
+            if(map.get(c)===0) map.delete(c)
+        }
+        if(map.size===1) return false
+        return true
+    }
+}
